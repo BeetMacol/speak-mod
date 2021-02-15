0bfd9a35117e59217922a3448d5462fd8d98ee5e
BeetMacol <beetmacol@gmail.com>
2021-02-15 21:54:06 +0100

    Add speaking keybinding and indicator

---
+++ src/main/java/com/beetmacol/speakmod/SpeachIndicatorRenderer.java
@@ -0,0 +1,38 @@
+package com.beetmacol.speakmod;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.util.math.MatrixStack;
+
+@Environment(EnvType.CLIENT)
+public class SpeachIndicatorRenderer implements HudRenderCallback {
+	static int frame = 0;
+	static float timer = 0f;
+
+	@Override
+	public void onHudRender(MatrixStack matrices, float tickDelta) {
+		MinecraftClient client = MinecraftClient.getInstance();
+		if (SpeakMod.keyBinding.isPressed() && (!client.isInSingleplayer() || client.player.networkHandler.getPlayerList().size() > 1)) {
+			int scaledWidth = client.getWindow().getScaledWidth();
+			int scaledHeight = client.getWindow().getScaledHeight();
+			int x = scaledWidth / 2 - 8;
+			int y = scaledHeight / 2 + 16;
+
+			timer += tickDelta;
+			if (timer > 7.5f) {
+				if (frame < 2) frame++;
+				else frame = 0;
+				timer -= 7.5f;
+			}
+
+			client.getTextureManager().bindTexture(SpeakMod.SPEACH_INDICATOR_TEXTURE);
+			RenderSystem.blendFuncSeparate(GlStateManager.SrcFactor.ONE_MINUS_DST_COLOR, GlStateManager.DstFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO);
+			DrawableHelper.drawTexture(matrices, x, y, 0, frame * 16, 16, 16, 16, 80);
+		}
+	}
+}
--- src/main/java/com/beetmacol/speakmod/SpeakMod.java
+++ src/main/java/com/beetmacol/speakmod/SpeakMod.java
@@ -1,16 +1,32 @@
 package com.beetmacol.speakmod;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
 
 public class SpeakMod {
+	public static final String MOD_ID = "speak-mod";
 	public static final Logger LOGGER = LogManager.getLogger("Speak Mod");
+
+	@Environment(EnvType.CLIENT)
+	public static KeyBinding keyBinding;
+	@Environment(EnvType.CLIENT)
+	public static final Identifier SPEACH_INDICATOR_TEXTURE = new Identifier(MOD_ID, "textures/gui/speach_indicator.png");
 
 	public void init() {
 		LOGGER.debug("Speak Mod initialized.");
 	}
 
 	public void clientInit() {
+		keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.speak-mod.speak", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT, "key.categories.multiplayer"));
+		HudRenderCallback.EVENT.register(new SpeachIndicatorRenderer());
 	}
 
 	public void serverInit() {
